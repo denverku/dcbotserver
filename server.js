b05936587a9b955de2b08a09405181cd54238246
KoolMohPC <justmohammad66@gmail.com>
2024-04-09 20:12:14 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const token = process.env.TOKEN;
+
+// listening to requests
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+
+// registering slash commands
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages],
+});
+
+client.once("ready", () => {
+  client.user.setActivity('with my slash commands',{ type:'PLAYING' });
+  console.log(`Ready on ${client.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.author.bot) return;
+  message.reply('I exist');
+})
+
+client.login(token);
+
+module.exports = app;
